5c1f7dc93f6629c37b262c2c0f051d60589bfa32
njacob1001 <njacob1001@gmail.com>
2019-11-23 13:59:38 -0500

    enable rfid reader

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	rfid "github.com/firmom/go-rfid-rc522/rfid"
+	rc522 "github.com/firmom/go-rfid-rc522/rfid/rc522"
+)
+
+func main() {
+	fmt.Println("RFID STARTED")
+	reader, err := rc522.NewRfidReader()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	readerChan, err := rfid.NewReaderChan(reader)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	rfidChan := readerChan.GetChan()
+	doSomethingCount := 0
+	for {
+		select {
+		case id := <-rfidChan:
+			fmt.Println(id)
+		default:
+			doSomethingCount++
+			fmt.Println(" ... ", doSomethingCount)
+			time.Sleep(1000 * time.Millisecond)
+		}
+	}
+}
